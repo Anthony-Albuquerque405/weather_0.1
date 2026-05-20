7d29606619eddb9bc6a817e19823599dc48872d5
Anthony <anthonyhitnailbr@gmail.com>
2026-05-20 15:09:39 -0300

    feat: melhorar exibição de informações climáticas e corrigir estrutura do componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Home() {
     if (!res.ok) {
       setLoading(false);
       setWethear(null);
-      setErro("erro");
+      setErro("erro"); // ta com string só para inicializar o ErroGet
       return;
     }
 
@@ -141,139 +141,140 @@ export default function Home() {
 
       {/* Resultado */}
 
-      {erro ? (
-        <ErrorGet />
-      ) : (
-        wethear && (
-          <div
-            className={`mt-10 w-full max-w-md md:max-w-2xl lg:max-w-4xl rounded-3xl shadow-lg p-6 transition-colors duration-300 ${
-              dark
-                ? "bg-linear-to-tr from-gray-700 via-gray-500 to-gray-200 text-gray-200"
-                : "bg-linear-to-tr from-blue-200 via-blue-300 to-blue-400 text-gray-800"
-            }`}
-          >
-            <div className="flex flex-col sm:flex-row items-center justify-center gap-3 mb-6">
-              <h1
-                className={`text-2xl sm:text-3xl font-extrabold tracking-wide text-center sm:text-left ${
-                  dark ? "text-white" : "text-blue-800"
-                }`}
-              >
-                {wethear.name}
-              </h1>
-              <img
-                className="w-12 h-8 rounded-md shadow-md border border-blue-200"
-                src={`https://flagcdn.com/w320/${wethear.sys.country.toLowerCase()}.png`}
-                alt={`Bandeira de ${wethear.sys.country}`}
-              />
-            </div>
-
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      {loading
+        ? searchLoading()
+        : erro
+          ? ErrorGet()
+          : wethear && (
               <div
-                className={`flex flex-col items-center rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-105 transition-transform duration-200 ${
+                className={`mt-10 w-full max-w-md md:max-w-2xl lg:max-w-4xl rounded-3xl shadow-lg p-6 transition-colors duration-300 ${
                   dark
-                    ? "bg-gray-700 text-gray-200"
-                    : "bg-white/70 text-gray-800"
+                    ? "bg-linear-to-tr from-gray-700 via-gray-500 to-gray-200 text-gray-200"
+                    : "bg-linear-to-tr from-blue-200 via-blue-300 to-blue-400 text-gray-800"
                 }`}
               >
-                <p className="text-lg sm:text-xl font-semibold">
-                  <Thermometer color="#d72828" /> {wethear.main.temp.toFixed(1)}
-                  °C
-                </p>
-                <span
-                  className={`text-xs sm:text-sm ${
-                    dark ? "text-gray-400" : "text-gray-600"
-                  }`}
-                >
-                  Temperatura
-                </span>
-              </div>
-
-              <div
-                className={`flex flex-col items-center rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-105 transition-transform duration-200 ${
-                  dark
-                    ? "bg-gray-700 text-gray-200"
-                    : "bg-white/70 text-gray-800"
-                }`}
-              >
-                <p className="text-lg sm:text-xl font-semibold">
-                  <ThermometerSun color="#e4f500" />{" "}
-                  {wethear.main.feels_like.toFixed(1)}°C
-                </p>
-                <span
-                  className={`text-xs sm:text-sm ${
-                    dark ? "text-gray-400" : "text-gray-600"
-                  }`}
-                >
-                  Sensação térmica
-                </span>
-              </div>
-
-              <div
-                className={`flex flex-col items-center rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-105 transition-transform duration-200 ${
-                  dark
-                    ? "bg-gray-700 text-gray-200"
-                    : "bg-white/70 text-gray-800"
-                }`}
-              >
-                <p className="text-lg sm:text-xl font-semibold flex flex-col items-center">
+                <div className="flex flex-col sm:flex-row items-center justify-center gap-3 mb-6">
+                  <h1
+                    className={`text-2xl sm:text-3xl font-extrabold tracking-wide text-center sm:text-left ${
+                      dark ? "text-white" : "text-blue-800"
+                    }`}
+                  >
+                    {wethear.name}
+                  </h1>
                   <img
-                    className="w-10 h-10 sm:w-12 sm:h-12 "
-                    src={`https://openweathermap.org/img/wn/${wethear.weather[0].icon}@2x.png`}
-                    alt=""
-                    style={{ filter: "drop-shadow(0 0 1px black)" }}
+                    className="w-12 h-8 rounded-md shadow-md border border-blue-200"
+                    src={`https://flagcdn.com/w320/${wethear.sys.country.toLowerCase()}.png`}
+                    alt={`Bandeira de ${wethear.sys.country}`}
                   />
-                  {wethear.weather[0]?.description}
-                </p>
-                <span
-                  className={`text-xs sm:text-sm ${
-                    dark ? "text-gray-400" : "text-gray-600"
-                  }`}
-                >
-                  Condição
-                </span>
-              </div>
+                </div>
 
-              <div
-                className={`flex flex-col items-center justify-center text-center rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-105 transition-transform duration-200 ${
-                  dark
-                    ? "bg-gray-700 text-gray-200"
-                    : "bg-white/70 text-gray-800"
-                }`}
-              >
-                <p className="text-lg sm:text-xl font-semibold">
-                  <Droplets color="#005dd6" /> {wethear.main.humidity}%
-                </p>
-                <span
-                  className={`text-xs sm:text-sm ${
-                    dark ? "text-gray-400" : "text-gray-600"
-                  }`}
-                >
-                  Umidade
-                </span>
-              </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                  <div
+                    className={`flex flex-col items-center rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-105 transition-transform duration-200 ${
+                      dark
+                        ? "bg-gray-700 text-gray-200"
+                        : "bg-white/70 text-gray-800"
+                    }`}
+                  >
+                    <p className="text-lg sm:text-xl font-semibold">
+                      <Thermometer color="#d72828" />{" "}
+                      {wethear.main.temp.toFixed(1)}
+                      °C
+                    </p>
+                    <span
+                      className={`text-xs sm:text-sm ${
+                        dark ? "text-gray-400" : "text-gray-600"
+                      }`}
+                    >
+                      Temperatura
+                    </span>
+                  </div>
 
-              <div
-                className={`col-span-1 sm:col-span-2 flex flex-col items-center rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-105 transition-transform duration-200 ${
-                  dark
-                    ? "bg-gray-700 text-gray-200"
-                    : "bg-white/70 text-gray-800"
-                }`}
-              >
-                <p className="text-lg sm:text-xl font-semibold">
-                  <Wind color="#005dd6" /> {wethear.wind.speed} m/s
-                </p>
-                <span
-                  className={`text-xs sm:text-sm ${
-                    dark ? "text-gray-400" : "text-gray-600"
-                  }`}
-                >
-                  Vento
-                </span>
+                  <div
+                    className={`flex flex-col items-center rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-105 transition-transform duration-200 ${
+                      dark
+                        ? "bg-gray-700 text-gray-200"
+                        : "bg-white/70 text-gray-800"
+                    }`}
+                  >
+                    <p className="text-lg sm:text-xl font-semibold">
+                      <ThermometerSun color="#e4f500" />{" "}
+                      {wethear.main.feels_like.toFixed(1)}°C
+                    </p>
+                    <span
+                      className={`text-xs sm:text-sm ${
+                        dark ? "text-gray-400" : "text-gray-600"
+                      }`}
+                    >
+                      Sensação térmica
+                    </span>
+                  </div>
+
+                  <div
+                    className={`flex flex-col items-center rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-105 transition-transform duration-200 ${
+                      dark
+                        ? "bg-gray-700 text-gray-200"
+                        : "bg-white/70 text-gray-800"
+                    }`}
+                  >
+                    <p className="text-lg sm:text-xl font-semibold flex flex-col items-center">
+                      <img
+                        className="w-10 h-10 sm:w-12 sm:h-12 "
+                        src={`https://openweathermap.org/img/wn/${wethear.weather[0].icon}@2x.png`}
+                        alt=""
+                        style={{ filter: "drop-shadow(0 0 1px black)" }}
+                      />
+                      {wethear.weather[0]?.description}
+                    </p>
+                    <span
+                      className={`text-xs sm:text-sm ${
+                        dark ? "text-gray-400" : "text-gray-600"
+                      }`}
+                    >
+                      Condição
+                    </span>
+                  </div>
+
+                  <div
+                    className={`flex flex-col items-center justify-center text-center rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-105 transition-transform duration-200 ${
+                      dark
+                        ? "bg-gray-700 text-gray-200"
+                        : "bg-white/70 text-gray-800"
+                    }`}
+                  >
+                    <p className="text-lg sm:text-xl font-semibold">
+                      <Droplets color="#005dd6" /> {wethear.main.humidity}%
+                    </p>
+                    <span
+                      className={`text-xs sm:text-sm ${
+                        dark ? "text-gray-400" : "text-gray-600"
+                      }`}
+                    >
+                      Umidade
+                    </span>
+                  </div>
+
+                  <div
+                    className={`col-span-1 sm:col-span-2 flex flex-col items-center rounded-xl p-4 shadow-sm hover:shadow-md hover:scale-105 transition-transform duration-200 ${
+                      dark
+                        ? "bg-gray-700 text-gray-200"
+                        : "bg-white/70 text-gray-800"
+                    }`}
+                  >
+                    <p className="text-lg sm:text-xl font-semibold">
+                      <Wind color="#005dd6" /> {wethear.wind.speed} m/s
+                    </p>
+                    <span
+                      className={`text-xs sm:text-sm ${
+                        dark ? "text-gray-400" : "text-gray-600"
+                      }`}
+                    >
+                      Vento
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        )
-      )}
+            )}
     </div>
   );
 }
